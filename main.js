995fcb570b887d9a7d9447f5968d0a1c227de22f
jet-kam <52491988+jet-kam@users.noreply.github.com>
2021-06-11 17:07:00 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import { runUI, addEmployeeUI, openTab, searchEmployeeUI } from './employees/ui';
+import { runUI, addEmployeeUI, openTab, searchEmployeeUI, showEmployees, fillselect } from './employees/ui';
 window.addEmployeeUI = addEmployeeUI;
 window.openTab = openTab;
 window.searchEmployeeUI = searchEmployeeUI;
+window.showEmployees = showEmployees;
 runUI();
+/* window.onload = runUI(); */
