e73c1802967cc52b2ba57d05fafc0c8de89e2b02
jet-kam <52491988+jet-kam@users.noreply.github.com>
2021-06-11 16:09:56 +0700

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-import { runUI, addEmployeeUI, openTab, searchEmployeeUI } from './employees/ui';
-window.addEmployeeUI = addEmployeeUI;
-window.openTab = openTab;
-window.searchEmployeeUI = searchEmployeeUI;
-runUI();
